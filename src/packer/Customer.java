a2a86ad3259f888b1afaa5fbbc0b7b67f11f9c99
RobTutauha <rob.tutauha@gmail.com>
2020-01-13 22:44:33 +1300

    Commented all the customer class methods

--- src/packer/Customer.java
+++ src/packer/Customer.java
@@ -6,26 +6,52 @@ import java.util.List;
 /**
  *
  * @author I.M.Bad
+ * 
+ * customer class defines customer info
+ * contains the following methods:
+ * Customer(String name, Address address)
+ * addAddress(Address address
+ * getName()
+ * getClosestAddressTo(Depot d)
+ * toString()
  */
 public class Customer {
     
     private String name;
     private List<Address> addresses;
 
+    /**
+     * Customer Constructor
+     * @param name string
+     * @param address address
+     */
     public Customer(String name, Address address) {
         addresses = new ArrayList<>();
         this.name = name;
         this.addresses.add(address);
     }
     
+    /**
+     * adds address to customer address list
+     * @param address address
+     */
     public void addAddress(Address address) {
         this.addresses.add(address);
     }
     
+    /**
+     * gets customer name
+     * @return string
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * calculates customer address closest to depot
+     * @param d depot
+     * @return string
+     */
     public Address getClosestAddressTo(Depot d) {
         double bestDistance = Double.MAX_VALUE;
         Address bestAddress = null;
@@ -38,6 +64,11 @@ public class Customer {
         return bestAddress;
     }
 
+    //THIS TOSTRING IS PROBABLY HOW ALL THE OTHERS SHOULD BE WORKING
+    /**
+     * gets customer name
+     * @return string
+     */
     public String toString() {
         return this.getName();
     }
