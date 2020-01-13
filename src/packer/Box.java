254a8ec2d0ccbd5dc4b0d537d2757452d2c1d9c0
RobTutauha <rob.tutauha@gmail.com>
2020-01-13 22:24:47 +1300

    Commented all the box class methods

--- src/packer/Box.java
+++ src/packer/Box.java
@@ -3,6 +3,20 @@ package packer;
 /**
  *
  * @author I.M.Bad
+ * 
+ * Defines and exposes box info
+ * runs the following methods:
+ * Box(Customer customer, Depot depot)
+ * addProduct(Product product)
+ * addProduct(Product product, int quantity)
+ * getLabel()
+ * toString()
+ * getWeight()
+ * canFit(Product p)
+ * canFit(Product p, int quantity)
+ * remainingCapacity()
+ * isFragile()
+ * isHazardous()
  */
 public class Box {
     
@@ -11,12 +25,21 @@ public class Box {
     private Customer customer;
     private Depot depot; 
 
+    /**
+     * Box Constructor
+     * @param customer customer
+     * @param depot depot
+     */
     public Box(Customer customer, Depot depot) {
         this.customer = customer;
         this.depot = depot;
         contents = new Manifest();
     }
     
+    /**
+     * Checks there is room and puts one product in the box
+     * @param product product
+     */
     public void addProduct(Product product) {
         if (canFit(product)) {
             contents.addProduct(product, 1);
@@ -24,13 +47,21 @@ public class Box {
     }
     
     
-    
+    /**
+     * Checks there is room and puts products in the box
+     * @param product product
+     * @param quantity quantity
+     */
     public void addProduct(Product product, int quantity) {
         if (canFit(product,quantity)); {
             contents.addProduct(product, quantity);
         }
     }
    
+    /**
+     * Calculates nearest address to depot and creates label accordingly
+     * @return 
+     */
     public String getLabel() {
         StringBuilder label = new StringBuilder();
         label.append(customer);
@@ -44,31 +75,63 @@ public class Box {
         }
         return label.toString();
     }
-    
+    //ABOVE AND BELOW ARE CIRCULAR?
+    /**
+     * SEEMS TO RUIN MY BRAIN
+     * @return 
+     */
     public String toString() {
         return getLabel();
     }
     
+    /**
+     * retrieves box weight
+     * @return TO BE DETERMINED
+     */
     public double getWeight() {// determine what class holds weight information, create appropriate method to determine weight and tie it to this method
         return contents.getTotalWeight();
     }
     
+    /**
+     * determines if single product will fit in the box
+     * @param p product
+     * @return boolean
+     */
     public boolean canFit(Product p) {
-        return p.getWeight() < 40; //All 40kgs should be 20. Should probably be a maxWeight variable so can change easily
+        return p.getWeight() < 40; //assuming this is kgs, All 40kgs should be 20. Should probably be a variable so can change easily
     }
     
+    /**
+     * determines if products will fit in the box
+     * @param p product
+     * @param quantity quantity
+     * @return boolean
+     */
     public boolean canFit(Product p, int quantity) {
         return (p.getWeight() * quantity) < 40;
     }
     
+    /**
+     * determines room left in the box
+     * @return integer
+     */
     public double remainingCapacity() {
         return 40 - this.getWeight();
     }
     
+    /**
+     * checks box for fragile items
+     * @return boolean
+     */
     public boolean isFragile() {
         return contents.hasFragileItems();
     }
     
+    //ALL HAZARDOUS ANYTHINGS IN THIS PROGRAM ARE PERMANENTLY FALSE WTF?
+    /**
+     * checks box for hazardous items
+     * @return boolean
+     */
     public boolean isHazardous() {
         return false;
     }
