4de89b892c1e64e69762c1cef2d5a018e06cf310
RobTutauha <rob.tutauha@gmail.com>
2020-01-13 22:36:22 +1300

    Commented all the coordinates class methods

--- src/packer/Coordinates.java
+++ src/packer/Coordinates.java
@@ -3,25 +3,45 @@ package packer;
 /**
  *
  * @author I.M.Bad
+ * 
+ * coordinates class defines and calculates coordinate info
  */
 public class Coordinates {
     
     private final double x;
     private final double y;
     
+    /**
+     * Coordinate Constructor
+     * @param x double
+     * @param y double
+     */
     public Coordinates(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * get the x coordinate
+     * @return double
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * get the y coordinate
+     * @return double
+     */
     public double getY() {
         return y;
     }
     
+    /**
+     * calculates euclidean distance
+     * @param other coordinates
+     * @return double
+     */
     public double euclideanDistanceTo(Coordinates other) {
         double xDiff = other.getX() - this.getX();
         double yDiff = other.getY() - this.getY();
@@ -29,6 +49,11 @@ public class Coordinates {
         return dist;
     }
     
+    /**
+     * calculates manhattan distance
+     * @param other coordinates
+     * @return double
+     */
     public double manhattanDistanceTo(Coordinates other) {
         double xDiff = other.getX() - this.getX();
         double yDiff = other.getY() - this.getY();
@@ -36,6 +61,11 @@ public class Coordinates {
         return dist;
     }
     
+    /**
+     * calculates company distance
+     * @param other coordinates
+     * @return double
+     */
     public double companyDistanceTo(Coordinates other) {
         double xDiff1 = other.getX() - this.getX();
         double yDiff1 = other.getY() - this.getY();
