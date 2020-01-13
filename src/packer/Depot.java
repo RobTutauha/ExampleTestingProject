c7edec549e2d755782a3d5d4bf9a58b323d5de53
RobTutauha <rob.tutauha@gmail.com>
2020-01-13 22:55:27 +1300

    Commented all the depot class methods

--- src/packer/Depot.java
+++ src/packer/Depot.java
@@ -3,24 +3,43 @@ package packer;
 /**
  *
  * @author I.M.Bad
+ * 
+ * depot class defines Depot info
  */
 public class Depot {
     private String name;
     private Address address;
 
+    /**
+     * depot constructor
+     * @param name string
+     * @param address address
+     */
     public Depot(String name, Address address) {
         this.name = name;
         this.address = address;
     }
     
+    /**
+     * gets depot name/address
+     * @return string
+     */
     public String getName() {
         return address.toString();
     }
     
+    /**
+     * get depot address coordinates
+     * @return coordinates
+     */
     public Coordinates getCoordinates() {
         return this.address.getCoordinates();
     }
     
+    /**
+     * get name/address
+     * @return string
+     */
     public String toString() {
         return this.getName();
     }
