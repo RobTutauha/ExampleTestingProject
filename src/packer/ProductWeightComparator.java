59da811e78237d32ea6997edc20eee81d3f5c01a
RobTutauha <rob.tutauha@gmail.com>
2020-01-13 21:50:35 +1300

    Commented all the product weight comparator class methods

--- src/packer/ProductWeightComparator.java
+++ src/packer/ProductWeightComparator.java
@@ -10,6 +10,12 @@ import java.util.Comparator;
  */
 public class ProductWeightComparator implements Comparator<Product> {
     
+    /**
+     * compares the weights of 2 products and returns +ve or -ve value
+     * @param a Product
+     * @param b Product
+     * @return positive or negative integer
+     */
     public int compare(Product a, Product b) {
         if (a.getWeight() < b.getWeight()) {return 1;}
         else if (a.getWeight() > b.getWeight()) {return -1;}
