dfb1c11a8800c273f89b9d88ee5844dee5eaaf3e
RobTutauha <rob.tutauha@gmail.com>
2020-01-14 17:27:04 +1300

    Added some printlns so I could see what is happening better.

--- src/packer/Packer.java
+++ src/packer/Packer.java
@@ -25,18 +25,22 @@ public class Packer {
         while (!m.isEmpty()) { // repeat until all items are packed
             if (b == null) {
                 b = new Box(c,d);
+                System.out.println("\nNew Box Built.");
             }
             // Pick the heaviest item in the manifest that will fit in the box
             Product prodToAdd = m.getHeaviestUnder(b.remainingCapacity());
             // Stop if no products left that will fit in the box
             if (prodToAdd == null) { 
                 packedBoxes.add(b);
+                System.out.println("\nBox Packed.");
                 b = null;
             }
             // Else put a product in the box
             else {
                 b.addProduct(prodToAdd);
+                System.out.println("\nAdding Product to box.");
                 m.removeProduct(prodToAdd);
+                System.out.println("\nRemoving Product from manifest.");
             }  
         }
         // Once finished, add last box to packedBoxes (WHY TWICE??)
@@ -44,6 +48,7 @@ public class Packer {
             packedBoxes.add(b);
             packedBoxes.add(b);
         }
+        System.out.println("\nBoxes Loaded.");
         return packedBoxes;
     }
     
