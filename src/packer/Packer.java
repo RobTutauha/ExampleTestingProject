15cf7c089f2e8eabfb37536e94d8a1eaa4d63e64
RobTutauha <rob.tutauha@gmail.com>
2020-01-17 11:52:53 +1300

    Refactored Packer class packProducts(). Renamed boxFull() to packBox() Renamed boxBuilder() to buildBox().
    Refactored packProducts to reuse boxFull.

--- src/packer/Packer.java
+++ src/packer/Packer.java
@@ -10,6 +10,12 @@ import java.util.List;
  * Packer class puts products in boxes and updates the manifest
  */
 public class Packer {
+    
+    private static int boxCounter = 1; // numbers the boxes
+    private static Box b = null; // box for products
+    
+    // List of boxes of products
+    private static List<Box> packedBoxes = new ArrayList<>();
 
     /**
      * Packs boxes and updates the manifest
@@ -19,17 +25,12 @@ public class Packer {
      * @param m Manifest manifest
      * @return List<Box> packedBoxes
      */
-    static int boxCounter = 1;
-    static Box b = null;
-    
     public static List<Box> packProducts(Customer c, Depot d, Manifest m) {
-        
-        List<Box> packedBoxes = new ArrayList<>();
         
         while (!m.isEmpty()) { // repeat until all manifest items are packed
             
-            if (b == null) { //if there's no box, create a box
-                b = boxBuilder(b, c, d);
+            if (b == null) { //if there's no box, build a box
+                b = buildBox(b, c, d);
             }
             
             // Pick the heaviest item in the manifest that will fit in the box
@@ -38,47 +39,42 @@ public class Packer {
             // Put a product in the box
             b.addProduct(prodToAdd);
             
-            //remove product from manifest
+            // Remove product from manifest
             m.removeProduct(prodToAdd);
 
             // Stop if no products left that will fit in the box
             if ((b.remainingCapacity() < 1) || (prodToAdd == null)) {
-                packedBoxes.add(b);
-//                System.out.println("\nMANIFEST still contains the following:");
-//                System.out.println(m);
-                System.out.println("Box " + boxCounter + " Packed with " + b.remainingCapacity() + "kg remaining");
-
-                boxCounter += 1; 
-                b = null;
+                b = packBox(b, m, packedBoxes);
             }
-
-            prodToAdd = null;
             
+            // Reset prodToAdd
+            prodToAdd = null;
         }
-        // Once finished, add last box to packedBoxes
         
+        
+        // Once manifest is empty, add last box to packedBoxes
         if (b != null) {
-            boxFull(b, m, packedBoxes);
+            packBox(b, m, packedBoxes);
         }
+        
         System.out.println("\nBoxes Loaded.");
         return packedBoxes;
     }
     
-    private static void boxFull(Box b, Manifest m, List<Box> packedBoxes) {
-        
+    
+    private static Box packBox(Box b, Manifest m, List<Box> packedBoxes) {
         packedBoxes.add(b);
-        if (m.isEmpty()) {
-            System.out.println("\nMANIFEST is EMPTY");
-        }
-        System.out.println("\nBox " + boxCounter + " Packed with " + b.remainingCapacity() + "kg remaining");
+        System.out.println("Box " + boxCounter + " Packed with " + b.remainingCapacity() + "kg remaining");
+        boxCounter += 1; 
         b = null;
+        return b;
     }
-
-    private static Box boxBuilder(Box b, Customer c, Depot d) {
+    
+    
+    private static Box buildBox(Box b, Customer c, Depot d) {
 
         b = new Box(c,d);
         System.out.println("\nBox " + boxCounter + " Built.");
-
         return b;
     }
     
