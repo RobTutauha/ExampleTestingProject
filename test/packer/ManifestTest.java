49fde19f919bec64c1251ce90af3e3a3613b4212
RobTutauha <rob.tutauha@gmail.com>
2020-01-17 08:53:20 +1300

    Created ManifestTest class

---
+++ test/packer/ManifestTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+/**
+ *
+ * @author RobT
+ */
+public class ManifestTest {
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("Testing Manifest class...");
+    }
+    
+    /**
+     * Test of getX method, of class Coordinates.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+
+    }
+    
+}
+
