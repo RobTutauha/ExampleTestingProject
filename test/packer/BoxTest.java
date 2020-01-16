ce3b0a6e9ffba8df2549f7e9babacc45b450639c
RobTutauha <rob.tutauha@gmail.com>
2020-01-17 08:53:35 +1300

    Created BoxTest class

---
+++ test/packer/BoxTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+/**
+ *
+ * @author MOTU
+ */
+public class BoxTest {
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("Testing Box class...");
+    }
+    
+    /**
+     * Test of getX method, of class Coordinates.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+
+    }
+}
